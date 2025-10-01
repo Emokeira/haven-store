857a9a8bb8e3da9d9132fca4bae85368a160a639
Edith Mokeira <edithmokeira7@gmal.com>
2025-10-01 21:45:56 +0300

    feat(app): wire ProtectedRoute into routing and secure pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,37 +6,65 @@ import Orders from "./pages/Orders";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Checkout from "./pages/Checkout";
-import About from "./pages/About"; // 👈 new page
+import About from "./pages/About"; 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute"; // 👈 we'll create this
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <div className="min-h-screen bg-background flex flex-col">
-          {/* Navbar (always visible) */}
-          <Navbar />
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <div className="min-h-screen bg-background flex flex-col">
+            {/* Navbar (always visible) */}
+            <Navbar />
 
-          {/* Page Content */}
-          <div className="flex-1 px-4 py-6">
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/about" element={<About />} /> {/* 👈 new route */}
-            </Routes>
+            {/* Page Content */}
+            <div className="flex-1 px-4 py-6">
+              <Routes>
+                {/* Public routes */}
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/about" element={<About />} />
+
+                {/* Protected routes */}
+                <Route
+                  path="/cart"
+                  element={
+                    <ProtectedRoute>
+                      <Cart />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/orders"
+                  element={
+                    <ProtectedRoute>
+                      <Orders />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/checkout"
+                  element={
+                    <ProtectedRoute>
+                      <Checkout />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
+            </div>
+
+            {/* Footer (always at bottom) */}
+            <Footer />
           </div>
-
-          {/* Footer (always at bottom) */}
-          <Footer />
-        </div>
-      </Router>
-    </CartProvider>
+        </Router>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
